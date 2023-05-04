095805d0d18649aa4e1a6d1e26670c06d2c770de
fanatiker1990 <59375292+fanatiker1990@users.noreply.github.com>
2023-05-04 20:14:11 +0300

    1

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        switch (year) {
+            case 2000, 2004 -> System.out.println("leap");
+            case 1900 -> System.out.println("not leap");
+        }
     }
 }
